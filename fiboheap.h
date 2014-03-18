9ac2995c4e92030e4d1a845b4a9b675bfd1b3401
srinivasbalaji-1 <198srinivas@gmail.com>
2014-03-18 19:22:47 -0400

    adding functionality addSibling

--- fiboheap.h
+++ fiboheap.h
@@ -7,6 +7,8 @@ using namespace std;
 template<class T>
 
 class fiboheap{
+		
+		
     struct fibnode{
       int degree;
       fibnode* child;
@@ -24,7 +26,8 @@ class fiboheap{
 			fibnode->parent = NULL;
 			fibnode->childcut = false;
 			fibnode->degree = 0;
-			if(root==NULL)
+			h1->data = val; 
+ 			if(root==NULL)
 			{
 				root = newNode;
 				minRoot = newNode;
@@ -37,8 +40,26 @@ class fiboheap{
     }
 
     public void merge(fibnode* h1,fibnode* h2){
-
+    	if(h1->data >= h2->data)
+    	{
+    		addSibling(h2,h1);
+    		
+    	}
+    	else{
+    		addSibling(h1,h2);
+    	}
     }
+    
+    public void addSibling(fibnode* h1, fibnode* h2)
+    {
+    	
+    	h2->left = h1->child->left->right;
+    	h2->right = h1->child;
+    	h1->child->left->right = h2;
+    	
+    }
+    
+    
 
     public void removeMin(){
 

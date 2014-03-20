16c3c31fbfb4cefd1bb5546ec4f4c2e43ec5b770
srinivasbalaji-1 <198srinivas@gmail.com>
2014-03-20 00:11:47 -0400

    corrected errors

--- fiboheap.h
+++ fiboheap.h
@@ -27,74 +27,90 @@ class fiboheap{
       fibnode* parent;
       bool childcut;
       T data;
-      };
-      fqueue<fibnode *> nodeDegree[5000]; 
-      fibnode* minRoot;
-      fibnode* root;
+    };
+    fqueue<fibnode *> nodeDegree[1000]; 
+    fibnode* minRoot;
+    fibnode* root;
       
     public:
     
-    void insert(T val)
-    {
-	fibnode* newNode = new fibnode;
-        newNode->left = NULL;
-        newNode->right = NULL;
-	newNode->parent = NULL;
-	newNode->childcut = false;
-	newNode->degree = 0;
-	newNode->data = val; 
- 	if(minRoot==NULL)
-	{
-  	minRoot = newNode;
-	}
-	else
-	{
-		minRoot = (minRoot->data > newNode->data) ? newNode:minRoot;
-		addSibling(root,newNode);				
-	}
+  	void insert(T val)
+  	{
+			fibnode* newNode = new fibnode;
+      newNode->left = newNode;
+      newNode->right = newNode;
+			newNode->parent = NULL;
+			newNode->childcut = false;
+			newNode->degree = 0;
+			newNode->data = val; 
+ 			if(minRoot==NULL)
+			{
+				root = newNode;
+  			minRoot = newNode;
+			}
+			else
+			{
+				minRoot = (minRoot->data > newNode->data) ? newNode:minRoot;
+				addSibling(root,newNode);				
+			}
     }
 
     void merge(fibnode* h1,fibnode* h2)
     {
     	if(h1->data >= h2->data)
     	{
-        	if(h2->child!=NULL) 
-                    addSibling(h2->child,h1);
+        if(h2->child!=NULL) 
+          addSibling(h2->child,h1);
     		else
     		{
     			h2->degree = h2->degree+1;
     			h2->child = h1;
     		}
+    		h1->left->right = h1->right;
+    		h1->right->left = h1->left;
     	}
     	else
         {
     		if(h1->child!=NULL) 
-                    addSibling(h1->child,h1);
+          addSibling(h1->child,h1);
     		else
     		{
     			h1->degree = h1->degree +1;
     			h1->child = h2;
     		}
+    		h2->left->right = h2->right;
+    		h2->right->left = h2->left;
     		
     	}
     }
     
     void addSibling(fibnode* h1, fibnode* h2)
     {
-    	
-    	h2->left = h1->left;
-    	h2->right = h1;
-    	h2->parent = h1->parent;
-    	h1->parent->degree = h1->parent->degree +1;
-    	h1->->left->right = h2;    	
+    	if(h1!=NULL)
+    	{
+    		fibnode* lastH1 = h1->left;
+    		fibnode* lastH2 = h2->left;
+    		lastH1->right = h2;
+    		h2->left = lastH1;
+    		h1->left = lastH2;
+    		lastH2->right = h1; 
+    		if(h1->parent!=NULL)
+    		{
+    			h2->parent = h1->parent;
+    			h1->parent->degree = h1->parent->degree +1;
+    		}    		
+    	} 
+    	else
+    	{
+    		root = h2;    	
+    	}   	
     }
     
     
 
-    void removeMin(){
-    	fibnode* firstChild = minRoot->child;
-    	if(root!=NULL)
-    	{
+    void removeMin()
+    {
+    		fibnode* firstChild = minRoot->child;
     		fibnode* temp = firstChild;
     		while(temp->right=firstChild)
     		{
@@ -102,31 +118,30 @@ class fiboheap{
     			temp = temp->right;
     		}
     		temp->parent = NULL;
-    		addSibling(root,temp);
-    	}
+    		addSibling(root,firstChild);
     	
-    	minRoot->left->right = minRoot->right;
-    	minRoot->right->left = minRoot->left;
-    	minRoot = NULL;
-    	fibnode* firstNode = root;
-    	fibnode* temp = root;
-    	while(temp->right!=root)
-    	{
-    		minRoot = (minRoot!=NULL && temp->data < minRoot->data) ? temp:minRoot;
-    		if(minRoot==NULL) minRoot = temp;
-    		if(!nodeDegree[temp->degree].isEmpty())
+    	
+    		minRoot->left->right = minRoot->right;
+    		minRoot->right->left = minRoot->left;
+    		root = (minRoot==root && root->right!=root) ? minRoot->right:NULL;
+    		minRoot = NULL;
+    	
+    		fibnode* firstNode = root;
+    		fibnode* temp = root;
+    		while(temp->right!=firstNode)
     		{
-    			 merge(nodeDegree[temp->degree].front(),temp);
-    			 nodeDegree[temp->degree].pop();
+    			minRoot = (minRoot!=NULL && temp->data < minRoot->data) ? temp:minRoot;
+    			if(minRoot==NULL) minRoot = temp;
+    			if(!nodeDegree[temp->degree].isEmpty())
+    			{
+    			 	merge(nodeDegree[temp->degree].front(),temp);
+    			 	nodeDegree[temp->degree].pop();
+    			}
+    			else
+    			{
+    				nodeDegree[temp->degree].push(temp);
+    			}
     		}
-    		else{
-    			nodeDegree[temp->degree].push(temp);
-    		}
-    	}
-    		
-    	
-    	
-
     }
 
     void descreaseKey(fibnode* h,T key,T val){
@@ -145,7 +160,7 @@ class fiboheap{
 
 
    
-}
+};
 
 #endif
 
